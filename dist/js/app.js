0536b47bb7754a11ae762274ecf8d4bf360aacb6
Sarah-Maziers <sarah.maziers@student.hepl.be>
2021-03-03 13:20:14 +0100

    added one circle moving

--- dist/js/app.js
+++ dist/js/app.js
@@ -8,44 +8,50 @@
 /***/ (() => {
 
 var canvas = document.querySelector('canvas');
-var ySpeed = 3;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var ySpeed = 2 + Math.floor(Math.random() * 5);
+var xSpeed = 2 + Math.floor(Math.random() * 5);
 var position = {
   x: canvas.width / 2,
   y: canvas.height / 2
 };
 var radius = canvas.width / 10;
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-var colors = ['#40A497'];
+var colors = ['#40A497', 'black', 'red'];
 var ctx = canvas.getContext('2d');
 ctx.fillstyle = colors.sort(function () {
   return 0.5 - Math.random();
-});
+}); //moyen de tirer un evaleur aléatoire dans un array
 
 function draw() {
   ctx.beginPath();
-  ctx.arc(position.x / 2, position.y / 2, radius, 0, Math.PI);
+  ctx.arc(position.x, position.y, radius, 0, 2 * Math.PI);
   ctx.fill();
 }
-
-draw();
 
 function animate() {
   if (position.y + radius > canvas.height || position.y - radius < 0) {
     ySpeed = -ySpeed;
   }
 
+  if (position.x + radius > canvas.width || position.x - radius < 0) {
+    xSpeed = -xSpeed;
+  }
+
   position.y += ySpeed;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  position.x += xSpeed;
+  ctx.clearRect(0, 0, canvas.width, canvas.height); //on demande à Canvas de s'effacer sinon les cercles vont se dessiner les uns sur les autres
+
   draw();
   window.requestAnimationFrame(animate);
 }
 
+draw();
 animate();
 window.addEventListener('resize', function () {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  draw();
+  animate();
 });
 
 /***/ }),
--- src/app.js
+++ src/app.js
@@ -1,39 +1,44 @@
 let canvas= document.querySelector('canvas')
-let ySpeed=2+Math.floor(Math.random()*5)
+canvas.width=window.innerWidth
+canvas.height=window.innerHeight
+let ySpeed=2+Math.floor(Math.random()*5) //valeur comprise entre 2 et 6 car le math.floor arrondit vers le bas et math.random contient une valeur entre 0 et 1
 let xSpeed=2+Math.floor(Math.random()*5)
 const position={
     x:canvas.width/2,
     y:canvas.height/2
 }
 const radius= canvas.width/10
-canvas.width=window.innerWidth
-canvas.height=window.innerHeight
-const colors=['#40A497']
+
+const colors=['#40A497','black','red']
+
 let ctx=canvas.getContext('2d')
-ctx.fillstyle=colors.sort(()=>0.5-Math.random());
+ctx.fillstyle=colors.sort(()=>0.5-Math.random()); //moyen de tirer un evaleur aléatoire dans un array
 
 function draw(){
     ctx.beginPath()
-    ctx.arc(position.x/2,position.y/2,radius,0,Math.PI)
+    ctx.arc(position.x,position.y,radius,0,2*Math.PI)
     ctx.fill()
 }
-
-draw()
 
 function animate(){
     if(position.y + radius > canvas.height || position.y - radius < 0){
         ySpeed=-ySpeed
     }
+    if(position.x + radius > canvas.width || position.x - radius < 0){
+        xSpeed=-xSpeed
+    }
     position.y+=ySpeed
     position.x+=xSpeed
-    ctx.clearRect(0,0,canvas.width,canvas.height)
+    ctx.clearRect(0,0,canvas.width,canvas.height) //on demande à Canvas de s'effacer sinon les cercles vont se dessiner les uns sur les autres
     draw();
     window.requestAnimationFrame(animate);
 }
+
+draw()
 animate()
 
 window.addEventListener('resize',()=>{
     canvas.width=window.innerWidth
     canvas.height=window.innerHeight
-    draw()
+    animate()
     })
